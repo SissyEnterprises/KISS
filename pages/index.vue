2586b43e6a71caffc4837c9d5cee42f2ea2a473a
Sara Sissy <i.am.sara.the.sissy@gmail.com>
2021-11-23 16:50:05 +0100

    fix: removed image from start page box

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,31 @@
     <v-row class="fill-height" align="center">
       <v-col align="center">
         <v-card max-width="13cm" shaped>
-          <v-img :src="require('~/assets/images/pages/index/background.jpg')">
-            <v-container fluid class="pa-0 ma-0 fill-height">
-              <v-row no-gutters align="center" justify="center">
-                <v-col align="center">
-                  <h1
-                    style="margin-top: 1cm; padding-bottom: 0.5cm"
-                    class="text-center"
+          <v-container fluid class="pa-0 ma-0 fill-height">
+            <v-row no-gutters align="center" justify="center">
+              <v-col align="center">
+                <h1
+                  style="margin-top: 1cm; padding-bottom: 0.5cm"
+                  class="text-center"
+                >
+                  Sissy App
+                </h1>
+                <p>Welcome to your journey into sissyhood!</p>
+                <div style="margin-top: 1cm; margin-bottom: 1cm">
+                  <v-btn
+                    :to="localePath('get-started')"
+                    x-large
+                    rounded
+                    color="pink darken-1"
+                    >{{ $t('Get started') }}</v-btn
                   >
-                    Sissy App
-                  </h1>
-                  <p>Welcome to your journey into sissyhood!</p>
-                  <div style="margin-top: 1.5cm">
-                    <v-btn
-                      :to="localePath('get-started')"
-                      x-large
-                      rounded
-                      color="pink darken-1"
-                      >{{ $t('Get started') }}</v-btn
-                    >
-                    <v-btn x-large rounded @click="login">{{
-                      $t('Log in')
-                    }}</v-btn>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-img>
+                  <v-btn x-large rounded @click="login">{{
+                    $t('Log in')
+                  }}</v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>
